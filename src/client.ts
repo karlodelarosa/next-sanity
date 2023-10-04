13da252d5d28768f8954f5281fedc82a0f77828a
karlodelarosa <karlo.karlo59@gmail.com>
2023-10-04 22:23:04 +0800

    Sanity sample

--- src/client.ts
+++ src/client.ts
@@ -2,11 +2,12 @@ import {createClient} from 'next-sanity'
 
 const projectId = "ikt4ndqv" // "pv8y60vp"
 const dataset = "production"// "production"
-const apiVersion = '2023-05-03'
+const apiVersion = 'v2021-10-21'
 
 export const client = createClient({
     projectId,
     dataset,
     apiVersion,
     useCdn: true,
+    perspective: 'raw',
 })
